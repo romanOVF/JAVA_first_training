e49fc498f5c88129afe2bf4421227f0b1ffca651
romanOVF <43718503+romanOVF@users.noreply.github.com>
2018-09-30 14:31:54 +0500

    Add files via upload

---
+++ Hallo.java
@@ -0,0 +1,15 @@
+// Created sep 30 sun 2018
+// reset && javac -d bin Hallo.java && java -cp bin Hallo
+
+public class Hallo {
+  static String [] str = { "Hallo! ", "Welt." };
+  public static void main ( String [] args ) {
+    for ( String i : str )
+      System.out.print ( i );
+
+    System.out.println ();
+  }
+}
+
+/*
+*/
